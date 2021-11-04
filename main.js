4d8bbd1c38cf209fd853b4efde8057d57155bd3d
Ann <anna.kmeb@gmail.com>
2021-11-05 00:33:38 +0200

    added modal shop-card,fixed toggle for filter

---
+++ main.js
@@ -0,0 +1,19 @@
+function showFilterForCategory () {
+    const choosenCategory = document.querySelector('.prod-cat');
+
+    choosenCategory.addEventListener('click', function (e) {
+        const category = e.target.innerText;
+        const teaCategory = document.querySelector('.tea-filter');
+        const coffeeCategory = document.querySelector('.coffee-filter');
+
+        if (category.toLowerCase().includes('tea')) {
+            teaCategory.style.display = 'block';
+            coffeeCategory.style.display = 'none';
+        } else {
+            teaCategory.style.display = 'none';
+            coffeeCategory.style.display = 'block';
+
+        }
+    })
+}
+showFilterForCategory();

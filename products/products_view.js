de669a8a3d25e5d538747f91fa0fcb7f2840492f
Anton Zhyvotovskyi <zhyvotovskyi@gmail.com>
2021-11-08 20:29:16 +0200

    Render class for renderModal, renderProdCard

--- products/products_view.js
+++ products/products_view.js
@@ -1,6 +1,7 @@
 import View from '../common/view.js';
+import Render from '../common/render.js';
 
-export default class ProductsView extends View {
+export default class ProductsView extends View extends Render {
 
     domElements = [
         {
@@ -20,6 +21,8 @@ export default class ProductsView extends View {
     constructor ( changeSearch, openModal ) {
         super();
         this.linkDomElem()
+        this.renderModal()
+        this.renderProdCard()
         this.dom.searchInput.addEventListener('change', changeSearch);
         this.openModal = openModal;
     }
@@ -47,97 +50,7 @@ export default class ProductsView extends View {
         this.dom.productModal.innerHTML = modalHTML;
     }
 
-    renderModal ( { id, name, region, weight, price, image, description, types, kind, manufactures, packages, category, roast} ) {
-        if ( category === 'coffee') {
-            types = roast;
-        }
-        return `<div id="id01" class="w3-modal" style="display: block;">
-                    <div class="w3-modal-content">
-                        <span
-                            onclick="document.getElementById('id01').style.display='none';"
-                            class="w3-button w3-display-topright"
-                            >&times;</span
-                        >
-                        <div class="w3-container">
-                            <div class="modal-text">
-                                <br />
-                                <h5 class="title row justify-content-center">
-                                    ${ name }
-                                </h5>
-                                <div class="row">
-                                    <div class=" col-6">
-                                        <img class="modal-prod-picture" src="${ image }">
-                                    </div>
-                                    <div class="modal-prod-description col-6">
-                                        <p class="modal-text-paragraph"> 
-                                            ${ description }
-                                        </p>
-                                        <div class="product_meta row">
-                                            <span class="posted_in">
-                                                <strong>Categories:</strong>
-                                                ${ types }, ${ kind } 
-                                            </span>
-                                            <span class="tagged_as">
-                                                <strong>Company:</strong> 
-                                                ${ manufactures } , ${ region }
-                                            </span>
-                                        </div>
-                                        <div class="m-bot15 text-center">
-                                            <br>
-                                        </div>
-                                        <div class="form-group text-center">
-                                            <label>Quantity</label>
-                                            <input type="quantiy" placeholder="1" class="form-control quantity input-center"/><span class="pro-price"> $ ${ price }</span>
-                                        </div>
-                                        <p class="text-center">
-                                            <button class="btn btn-round btn-danger" type="button">
-                                                <i class="fa fa-shopping-cart"></i> Add to Cart
-                                            </button>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                class="btn btn-secondary"
-                                data-dismiss="modal"
-                                onclick="document.getElementById('id01').style.display='none'"
-                            >
-                                Close
-                            </button>
-                        </div>
-                    </div>
-                </div>`
-    }
-
-    renderProdCard ( { id, name, region, weight, price, image, description, types, kind, manufactures, packages, category, roast} ) {
-        if ( category === 'coffee') {
-            types = roast;
-        }
-        return `<div class="col-sm-6 col-lg-4 col-12">
-                    <section class="panel product-card">
-                        <div class="pro-img-box">
-                            <img src="${ image }" alt="image" class="d-flex justify-content-center"/>
-                            <a href="#" class="adtocart">
-                                <i class="fa fa-shopping-cart"></i>
-                            </a>
-                        </div>
-                        <div class="product-card-body">
-                                <h4>
-                                    <a class="btn btn-card product-card-title" data-product-id="${ id }">${ name }</a>
-                                </h4>
-                            <div class="text-center">
-                                <div class="descriptions">${ weight } g</div>
-                                <div class="descriptions">${ types }, ${ kind }</div>
-                                <div class="descriptions"><u>${ region }</u></div>
-                                <div class="descriptions">${ manufactures }</div>
-                            </div>
-                            <p class="price">${ price } $</p>
-                        </div>
-                    </section>
-                </div>`
-    }//document.getElementById('id01').style.display='block'
+    // TODO: kept it just for reference ↓↓↓↓↓↓↓↓
+    // renderModal({ id, name, region, weight, price, image, description, types, kind, manufactures, packages, category, roast });
+    // renderProdCard({ id, name, region, weight, price, image, description, types, kind, manufactures, packages, category, roast });
 }
--- searchbar.js
+++
@@ -1,6 +0,0 @@
-const productsList = document.getElementById('products-list-id');
-const searchInput = document.getElementById('search-input-id');
-
-searchInput.addEventListener('keyup', (e) => {
-    console.log(e.target.value);
-});

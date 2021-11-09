e236fa7f4b600b7d3a5ded956ea9c2c0241adc8a
Ann <anna.kmeb@gmail.com>
2021-11-09 22:57:38 +0200

    fixed paggination component

---
+++ pagination/pagination_controller.js
@@ -0,0 +1,24 @@
+import Publisher from '../common/publisher.js';
+import PaginationView from './pagination_view.js';
+
+export default class PaginationController {
+
+    constructor() {
+        this.view = new PaginationView( this.clickPagination );
+        Publisher.subscribe( Publisher.events.clickPagBtn, this.clickPagination);
+        Publisher.subscribe( Publisher.events.loadData, this.createPagBtn)
+    }
+
+    clickPagination = ( event ) => {
+        const count = this.view.clickOnPagBut( event );
+        Publisher.notify( Publisher.events.renderProdOnPage, count );
+    }
+
+    createPagBtn = ( length ) => {
+        this.view.setupPagination( length );
+    }
+
+
+}
+
+
